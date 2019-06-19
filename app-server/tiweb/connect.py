b20a6369d40b5c4496d7f25a86ef6775af74fb8d
James Schnebly <jameschnebly@gmail.com>
2019-06-19 10:30:39 -0700

    Demo change

--- app-server/tiweb/connect.py
+++ app-server/tiweb/connect.py
@@ -1,13 +1,17 @@
 from py2neo import Graph
 
 # development graph
-def connectDev():
-    graph = Graph("bolt://127.0.0.1:7687", auth=("neo4j", "wolfpack"))
-    return graph
+# def connectDev():
+#     graph = Graph("bolt://127.0.0.1:7687", auth=("neo4j", "wolfpack"))
+#     return graph
 
 # connect to user's individual container 
 def connectProd(user, passw, addr, port):
     URI = "bolt://" + addr + ":" + port
     graph = Graph(URI, auth=(user, passw))
     return graph
-
+    
+# dev prod connection 
+def connectDev():
+    graph = Graph("bolt://moose.soc.unr.edu:31413", auth=("neo4j", "dfKrTJN7"))
+    return graph
