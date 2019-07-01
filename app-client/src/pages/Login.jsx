b72e91845c6c71526924c8656e3157cd70f0a85e
Ankita H Thakkar <athakkar@090205639dhq2-l.rd.unr.edu>
2019-06-30 19:45:01 -0700

    modifications

--- app-client/src/pages/Login.jsx
+++ app-client/src/pages/Login.jsx
@@ -27,7 +27,7 @@ class Login extends Component {
 
     login(user).then(res => {
       if (!res.exit) {
-        this.props.history.push('/');
+        this.props.history.push('/graph');
       }
     });
   }
